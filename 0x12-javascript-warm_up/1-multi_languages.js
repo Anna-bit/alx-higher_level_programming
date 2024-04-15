9394f7902c2eaf0abedc654a520d4b34a414c1f4
Anna-bit <amusau4@gmail.com>
2024-04-15 11:14:43 -0700

    1-multi language

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+
+const myfirstline = 'C is fun';
+const mysecondline = 'Python is cool';
+const mythirdline = 'JavaScript is amazing';
+
+console.log(myfirstline);
+console.log(mysecondline);
+console.log(mythirdline);
